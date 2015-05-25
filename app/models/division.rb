c5f74e50c01c3075d3bc328db45c244c150a14a1
Shinsuke Nishio <nishio@densan-labs.net>
2015-05-26 01:13:42 +0900

    @@ignore add

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: divisions
+#
+#  id          :integer          not null, primary key
+#  version_id  :integer          not null
+#  name        :string(255)      not null
+#  description :text(65535)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Division < ActiveRecord::Base
 end
